12e303090d887e9f729d1e84118500dc38766d5f
JohnDaCruz <batatadoce31@hotmail.com>
2023-12-22 22:55:25 -0300

    nextconfig normal

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const headers = async () => {
-    return [
-        {
-            // Correto:
-            source: "https://server-socketio.onrender.com/", // Define para quais rotas aplicar os headers
-            headers: [
-                { key: "Access-Control-Allow-Origin", value: "*" },
-                { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-                { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-            ],
-        },
-    ];
-};
-
-const nextConfig = {
-    headers
-}
+const nextConfig = {}
 
 module.exports = nextConfig
