19dfe8ff384824cb652acb1681d8bc321dbd7642
JohnDaCruz <batatadoce31@hotmail.com>
2023-12-22 22:49:36 -0300

    socket url correct & nextConfig CORS

--- next.config.js
+++ next.config.js
@@ -2,21 +2,19 @@
 const headers = async () => {
     return [
         {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-        },
-        {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-        },
-        {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type",
+            // Correto:
+            source: "https://server-socketio.onrender.com/", // Define para quais rotas aplicar os headers
+            headers: [
+                { key: "Access-Control-Allow-Origin", value: "*" },
+                { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+                { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+            ],
         },
     ];
 };
+
 const nextConfig = {
-    headers,
+    headers
 }
 
 module.exports = nextConfig
