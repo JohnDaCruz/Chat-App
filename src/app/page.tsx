917229ba4fac506596f75ff3ec325989813c8f98
JohnDaCruz <batatadoce31@hotmail.com>
2024-01-19 18:34:52 -0300

    login page modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,54 +40,55 @@ export default function Home() {
 
     if(!session){
         return (
-            <div className="container mx-auto p-4">
-                {loading && <Loader/>}
-                <h1 className="text-3xl font-bold mb-4">Welcome</h1>
-                <form className="flex flex-col" onSubmit={handleSubmit}>
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email</label>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Digite seu email..."
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        autoComplete={"username"}
-                        className="mt-1 p-2 w-full border rounded-md"
-                        disabled={loading}
-                    />
+            <div className="flex items-center justify-center h-screen bg-[#404040]">
+                <div className="flex flex-col p-5 bg-[#242521] w-[500px] rounded-2xl">
+                    <form onSubmit={handleSubmit}>
+                        {loading && <Loader/>}
+                        <label htmlFor="email" className="block text-lg font-medium text-white">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Digite seu email..."
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            autoComplete={"username"}
+                            className="mt-1 mb-5 p-2 w-full border rounded-md"
+                            disabled={loading}
+                        />
 
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-600">password</label>
-                    <input
-                        autoComplete="current-password"
-                        type="password"
-                        name="password"
-                        placeholder="Digite uma password.."
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="mt-1 p-2 w-full border rounded-md"
-                        disabled={loading}
-                    />
-                    {errorSignin && <p className={"text-red-600 mb-2"}>Email e(ou) senha incorretos</p>}
+                        <label htmlFor="password" className="block text-lg font-medium text-white">Senha</label>
+                        <input
+                            autoComplete="current-password"
+                            type="password"
+                            name="password"
+                            placeholder="Digite uma password.."
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            disabled={loading}
+                        />
+                        {errorSignin && <p className={"text-red-600 mb-2"}>Email e(ou) senha incorretos</p>}
+
+                        <button
+                            disabled={loading}
+                            type="submit"
+                            className="bg-[#5D6DFF] text-white p-2 mt-2 rounded-md mx-auto block w-3/4 sm:w-1/2 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                        >
+                            Login
+                        </button>
+                    </form>
 
                     <button
                         disabled={loading}
-
-                        type="submit"
-                        className="bg-blue-500 text-white px-2 py-2 mb-2 rounded-md mx-auto block w-3/4 sm:w-1/2 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                        onClick={() => signIn('google')}
+                        className="bg-[#5D6DFF] text-white p-2 mt-2 rounded-md mx-auto block w-3/4 sm:w-1/2 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
                     >
-                        Sign In
+                        Signin with Google
                     </button>
 
-                </form>
-
-                <button
-                    disabled={loading}
-                    onClick={() => signIn('google')}
-                    className="bg-blue-500 text-white px-2 py-2 mb-2 rounded-md mx-auto block w-3/4 sm:w-1/2 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-                >
-                    GOOGLE WITH GOOGLE
-                </button>
+                </div>
             </div>
+
         );
     }
 }
